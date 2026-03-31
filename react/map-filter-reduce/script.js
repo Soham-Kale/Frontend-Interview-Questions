f28e848e987996a08ee35e9b8aaacd65e4c2ae52
Soham-Kale <sohamkale3014@gmail.com>
2026-03-31 22:34:32 +0530

    map, filter & reduce

---
+++ react/map-filter-reduce/script.js
@@ -0,0 +1,46 @@
+
+const products = [
+    { name: 'Laptop', price: 1000, category: 'Electronics' },
+    { name: 'Phone', price: 500, category: 'Electronics' },
+    { name: 'Shirt', price: 50, category: 'Clothing' },
+    { name: 'Pants', price: 80, category: 'Clothing' },
+    { name: 'Headphones', price: 200, category: 'Electronics' },
+]
+
+
+function PrintAllProducts() {
+    return (
+        <div>
+            <h1>Question 1: Rerendering Conditions and Operations</h1>
+
+            {
+                products.map((product, index) => { // id
+                    return (
+                        <h4 key={index}>Product name: {product.name} with price {product.price} and category is {product.category}</h4>
+                    )
+                })
+            }
+
+            <h2>Question 2: How can filter products with specific category</h2>
+
+            {
+                products.filter((product) => (product.category === 'Electronics')).map((product, index) => {
+                    return (
+                        <h4 key={index}>Product name: {product.name} with price {product.price} and category is {product.category}</h4>
+                    )
+                })
+            }
+
+            <h2>Question 3: How can you summery of total prices for products</h2>
+            {
+                products.reduce((acc, product) => { 
+                    return acc + product.price
+                }, 0)
+            }
+
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("title"));
+root.render(React.createElement(PrintAllProducts));
