8a5ced75ca0ba6d0ae5913a480aa3b20d4dae1e2
Soham-Kale <sohamkale3014@gmail.com>
2026-04-03 22:47:04 +0530

    Implement UseEffectHook with data fetching and layout effect explanation

--- react/hooks/src/App.jsx
+++ react/hooks/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import UseEffectHook from './components/useeffect'
 import UseStateHook from './components/usestate'
 
 function App() {
   return (
     <>
       <h1>Hello Soham</h1>
-      <UseStateHook />
+      {/* <UseStateHook /> */}
+      <UseEffectHook/>
     </>
   )
 }
--- react/hooks/src/components/useeffect.jsx
+++ react/hooks/src/components/useeffect.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 
-const useEffect = () => {
+const UseEffectHook = () => {
+    const [userData, setUserData] = useState([]);
+
+    const featchData = async() => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await response.json();
+        setUserData(data);
+        // console.log("response data: ", data);
+    }
+
+    useEffect(() => {
+
+    }, []);
+
+    useLayoutEffect(() => {
+        
+    }, [])
+
     return (
-        <div>useEffect</div>
+        <div>
+            <button onClick={featchData}>Featch Data</button>
+
+            {
+                userData.map((user) => (
+                    <div key={user.id}>
+                        <h1>{user.name}</h1>
+                        <h3>{user.website}</h3>
+                    </div>
+                ))
+            }
+
+            <h5>
+                Question 6: Explain useLayoutEffect and how it is different from useEffect?
+            </h5>
+
+            {/* useEffect:
+            - Asynchronous: Runs after the render cycle is committed to the screen.
+            - Good for Performance: Does not block the browser from painting changes on the screen.
+
+            useLayoutEffect:
+            - Synchronous: Runs synchronously immediately after the DOM is updated but before the browser paints anything on the screen.
+            - Potentially Blocking: Can potentially cause delays in the rendering process if the operations are heavy. 
+            */}
+        </div>
     )
 }
 
-export default useEffect;
+export default UseEffectHook;
