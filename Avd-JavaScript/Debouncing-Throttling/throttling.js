744df757b5b87403c46e9f3e1ccd8b71b621369c
Soham-Kale <sohamkale3014@gmail.com>
2026-07-31 12:17:09 +0530

    Add initial implementations for debouncing and throttling with explanatory comments

---
+++ Avd-JavaScript/Debouncing-Throttling/throttling.js
@@ -0,0 +1,2 @@
+// Throttling: Ensures that a function is called at most once in a specified time interval. 
+// This is useful for scenarios like window resizing or scrolling where you want to limit the number of times a function is called.
