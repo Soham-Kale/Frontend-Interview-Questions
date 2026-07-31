af2304e598d0276bf1518548b57a0d113ad12c00
Soham-Kale <sohamkale3014@gmail.com>
2026-07-31 12:17:02 +0530

    Add initial implementation for debouncing and throttling; include HTML, CSS, and JavaScript files

---
+++ Avd-JavaScript/Debouncing-Throttling/script.js
@@ -0,0 +1,6 @@
+// Debouncing and Throttling in javascript
+
+// ----------------- Que.1 -------------------------
+// Create button ui and add debounce as follows =>
+//      => show "Button Pressed <X> Times" every time when button is pressed
+//     => Increase "Triggered <Y> Times" count after 800ms debounce
---
+++ Avd-JavaScript/promise/polyfill.js
@@ -0,0 +1,37 @@
+// Promise polyfills
+
+function PromisePolyfill(executor) {
+    let onResolve, onReject;
+
+    function resolve(value) {
+        onResolve(value);
+    };
+
+    function reject(value) {
+        onReject(value);
+    };
+
+    this.then = function(callback) {
+        onResolve = callback;
+        return this;
+    }
+
+    this.catch = function(callback) {
+        onReject = callback;
+        return this;
+    }
+
+    executor(resolve, reject);
+};
+
+const examplePromise = new PromisePolyfill((resolve, reject) => {
+    setTimeout(() => {
+        resolve(2);
+    }, 1000);
+});
+
+examplePromise
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+
+
